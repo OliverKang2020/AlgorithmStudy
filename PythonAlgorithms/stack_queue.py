a959083fce883562c051e7a9ba01c0d7d9fad999
OliverKang <junho920219@gmail.com>
2022-10-08 17:58:18 +0900

    add bfs dfs practices

---
+++ PythonAlgorithms/stack_queue.py
@@ -0,0 +1,27 @@
+
+# Stack
+stack = list()
+
+stack.append(5)
+stack.append(2)
+stack.append(4)
+stack.pop()
+stack.append(3)
+stack.append(7)
+stack.pop()
+
+print(stack[::-1])
+
+
+# Queue
+queue = list()
+
+queue.append(5)
+queue.append(2)
+queue.append(4)
+queue.pop(0)
+queue.append(3)
+queue.append(7)
+queue.pop(0)
+
+print(queue)
